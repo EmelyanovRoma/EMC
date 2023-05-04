dcab4218d0549324ad5245afcccb642a3a4d5833
R.O.G <Romanter456@gmail.com>
2023-05-04 15:07:03 +0700

    feature(Geometry.py): добавлено несколько полей для класса, сеттеры и геттеры для них

--- src/EMC/EMC.View/Geometry.py
+++ src/EMC/EMC.View/Geometry.py
@@ -1,45 +1,70 @@
-#from typing import NamedTuple
-
-
 class GeometryBlock:
     def __init__(self,NameTMET,PatternidTMET,TypeTMET,Value1TMET,Value2TMET,Value3TMET,Value4TMET,
                  NameBMET,PatternidBMET,TypeBMET,Value1BMET,Value2BMET,Value3BMET,Value4BMET,
                  nlev,xwidth,ywidth,xcells2,ycells2,nsubs,eeff,
-                 thickness,erel,mrel,eloss,mloss,esignma,nzpart,name):
+                 thickness,erel,mrel,eloss,mloss,esignma,nzpart,name, LayType, LayName, Mapping, 
+                 Type, Ilevel, Nvertices, Mtype, Filltype, Debugid, Xmin, Ymin, Xmax, Ymax, Conmax1, 
+                 Res1, Res2, Edgemesh, ToLevel, Conmax2, Meshingfill, Pads, X, Y, UL):
         #TMET
         self.__NameTMET = NameTMET
-        self.__PatternidTMET  =   PatternidTMET
-        self.__TypeTMET   =   TypeTMET
-        self.__Value1TMET =   Value1TMET
-        self.__Value2TMET =   Value2TMET
-        self.__Value3TMET =   Value3TMET
-        self.__Value4TMET =   Value4TMET
+        self.__PatternidTMET = PatternidTMET
+        self.__TypeTMET = TypeTMET
+        self.__Value1TMET = Value1TMET
+        self.__Value2TMET = Value2TMET
+        self.__Value3TMET = Value3TMET
+        self.__Value4TMET = Value4TMET
         #BMET
-        self.__NameBMET   =  NameBMET
-        self.__PatternidBMET  =   PatternidBMET
-        self.__TypeBMET   =   TypeBMET
-        self.__Value1BMET =   Value1BMET
-        self.__Value2BMET =   Value2BMET
-        self.__Value3BMET =   Value3BMET
-        self.__Value4BMET =   Value4BMET
+        self.__NameBMET = NameBMET
+        self.__PatternidBMET = PatternidBMET
+        self.__TypeBMET = TypeBMET
+        self.__Value1BMET = Value1BMET
+        self.__Value2BMET = Value2BMET
+        self.__Value3BMET = Value3BMET
+        self.__Value4BMET = Value4BMET
         #BOX
-        self.__nlev   =  nlev
-        self.__xwidth  =   xwidth
-        self.__ywidth   =   ywidth
-        self.__xcells2 =   xcells2
-        self.__ycells2 =   ycells2
-        self.__nsubs =   nsubs
-        self.__eeff =   eeff
+        self.__nlev = nlev
+        self.__xwidth = xwidth
+        self.__ywidth = ywidth
+        self.__xcells2 = xcells2
+        self.__ycells2 = ycells2
+        self.__nsubs = nsubs
+        self.__eeff = eeff
         #Box
-        self.__thickness   =  thickness
-        self.__erel  =   erel
-        self.__mrel   =   mrel
-        self.__eloss =   eloss
-        self.__mloss =   mloss
-        self.__esignma =   esignma
-        self.__nzpart =   nzpart
-        self.__name =   name
-
+        self.__thickness = thickness
+        self.__erel = erel
+        self.__mrel = mrel
+        self.__eloss = eloss
+        self.__mloss = mloss
+        self.__esignma = esignma
+        self.__nzpart = nzpart
+        self.__name = name
+        #TECHLAY
+        self.__lay_type = LayType
+        self.__lay_name = LayName
+        self.__mapping = Mapping
+        self.__type = Type
+        self.__ilevel = Ilevel
+        self.__nvertices = Nvertices
+        self.__mtype = Mtype
+        self.__filltype = Filltype
+        self.__debugid = Debugid
+        self.__xmin = Xmin
+        self.__ymin = Ymin
+        self.__xmax = Xmax
+        self.__ymax = Ymax
+        self.__conmax1 = Conmax1
+        self.__res1 = Res1
+        self.__res2 = Res2
+        self.__edgemesh = Edgemesh
+        #TOLEVEL
+        self.__tolevel = ToLevel
+        self.__conmax2 = Conmax2
+        self.__meshingfill = Meshingfill
+        self.__pads = Pads
+        #LORGN
+        self.__x = X
+        self.__y = Y
+        self.__uL = UL
 
         #Box
     def Getthickness(self):
@@ -195,4 +220,124 @@ class GeometryBlock:
     def SetValue4BMET(self, Value4BMET):
         self.__Value4BMET = Value4BMET
 
+    #TECHLAY
+    def GetLayType(self):
+        return self.__lay_type
+    def SetLayType(self, LayType):
+        self.__lay_type = LayType
 
+    def GetLayName(self):
+        return self.__lay_name
+    def SetLayName(self, LayName):
+        self.__lay_name = LayName
+
+    def GetMapping(self):
+        return self.__mapping
+    def SetMapping(self, Mapping):
+        self.__mapping = Mapping
+
+    def GetType(self):
+        return self.__type
+    def SetType(self, Type):
+        self.__type = Type
+
+    def GetIlevel(self):
+        return self.__ilevel
+    def SetIlevel(self, Ilevel):
+        self.__ilevel = Ilevel
+
+    def GetNvertices(self):
+        return self.__nvertices
+    def SetNvertices(self, Nvertices):
+        self.__nvertices = Nvertices
+
+    def GetMtype(self):
+        return self.__mtype
+    def SetMtype(self, Mtype):
+        self.__mtype = Mtype
+
+    def GetFilltype(self):
+        return self.__filltype
+    def SetFilltype(self, Filltype):
+        self.__filltype = Filltype
+       
+    def GetDebugid(self):
+        return self.__debugid
+    def SetDebugid(self, Debugid):
+        self.__debugid = Debugid
+
+    def GetXmin(self):
+        return self.__xmin
+    def SetXmin(self, Xmin):
+        self.__xmin = Xmin
+
+    def GetYmin(self):
+        return self.__ymin
+    def SetYmin(self, Ymin):
+        self.__ymin = Ymin
+
+    def GetXmax(self):
+        return self.__xmax
+    def SetXmax(self, Xmax):
+        self.__xmax = Xmax
+
+    def GetYmax(self):
+        return self.__ymax
+    def SetYmax(self, Ymax):
+        self.__ymax = Ymax
+
+    def GetConmaxx1(self):
+        return self.__conmax1
+    def SetConmax1(self, Conmax1):
+        self.__conmax1 = Conmax1
+
+    def GetRes1(self):
+        return self.__res1
+    def SetRes1(self, Res1):
+        self.__res1 = Res1
+
+    def GetRes2(self):
+        return self.__res2
+    def SetRes2(self, Res2):
+        self.__res2 = Res2
+    
+    def GetEdgemesh(self):
+        return self.__edgemesh
+    def SetEdgemesh(self, Edgemesh):
+        self.__edgemesh = Edgemesh
+    #TOLEVEL
+    def GetToLevel(self):
+        return self.__tolevel
+    def SetToLevel(self, ToLevel):
+        self.__tolevel = ToLevel
+
+    def GetConmax2(self):
+        return self.__conmax2
+    def SetConmax2(self, Conmax2):
+        self.__conmax2 = Conmax2
+
+    def GetMeshingfill(self):
+        return self.__meshingfill
+    def SetMeshingfill(self, Meshingfill):
+        self.__meshingfill = Meshingfill
+
+    def GetPads(self):
+        return self.__pads
+    def SetPads(self, Pads):
+        self.__pads = Pads
+
+    #LORGN
+    def GetX(self):
+        return self.__x
+    def SetX(self, X):
+        self.__x = X
+    
+    def GetY(self):
+        return self.__y
+    def SetY(self, Y):
+        self.__y = Y
+    
+    def GetUL(self):
+        return self.__uL
+    def SetUL(self, UL):
+        self.__uL = UL
